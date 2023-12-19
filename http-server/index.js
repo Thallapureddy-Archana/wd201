400d3623f7b8def2825c7cc9878769f04ec5b716
Thallapureddy-Archana <139766023+Thallapureddy-Archana@users.noreply.github.com>
2023-12-19 20:55:17 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,68 +1,46 @@
+const args = require("minimist")(process.argv.slice(2));
+
 const http = require("http");
 const fs = require("fs");
 
-// Read the content of "home.html" once
-fs.readFile("home.html", (err, homeContent) => {
-  if (err) {
-    throw err;
-  }
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
 
-  // Create a server and respond with the content of "home.html"
-  http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(homeContent);
-    response.end();
-  }).listen(3000, () => {
-    console.log("Server is running on port 3000");
-  });
-});
-/*const http = require("http");
-const fs = require("fs");
 fs.readFile("home.html", (err, home) => {
-  console.log(home.toString());
+    if (err) throw err;
+    homeContent = home;
 });
+fs.readFile("project.html", (err, project) => {
+    if (err) throw err;
+    projectContent = project;
+});
+fs.readFile("registration.html", (err, registration) => {
+    if (err) throw err;
+    registrationContent = registration;
+})
 fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  http
-    .createServer((request, response) => {
-      response.writeHeader(200, { "Content-Type": "text/html" });
-      response.write(home);
-      response.end();
-    })
-    .listen(3000);
-});    
+    console.log(home.toString());
+});
 
-Save the file and run the same using npm start. Now, if you navigate to localhost:3000 you can see that the file is rendered as expected.
-
-The writeHeader function informs the Node.js HTTP server that the content should be served as HTML. The write function transmits the content received from readFile.
-
-But clicking on the Projects link doesn't take you anywhere and the browser shows a different URL but nothing loads. Let us fix that.
-
-const fs = require("fs");
-fs.writeFile(
-    "sample.txt",
-    "Hello World. Welcome to Node.js File System module.",
-    (err) => {
-      if (err) throw err;
-      console.log("File created!");
-    }
-  );
-  fs.readFile("sample.txt", (err, data) => {
+fs.readFile("home.html", (err, home) => {
     if (err) throw err;
-    console.log(data.toString());
-  });
-  fs.appendFile("sample.txt", " This is my updated content", (err) => {
-    if (err) throw err;
-    console.log("File updated!");
-  });
-  fs.rename("sample.txt", "test.txt", (err) => {
-    if (err) throw err;
-    console.log("File name updated!");
-  });
-  fs.unlink("test.txt", (err) => {
-    if (err) throw err;
-    console.log("File test.txt deleted successfully!");
-  });*/
-  
+    http.createServer((request, response) => {
+        let url = request.url;
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        switch (url) {
+            case "/registration":
+                response.write(registrationContent);
+                response.end();
+                break;
+            case "/project":
+                response.write(projectContent);
+                response.end();
+                break;
+            default:
+                response.write(homeContent);
+                response.end();
+                break;
+        }
+    }).listen(args);
+})
